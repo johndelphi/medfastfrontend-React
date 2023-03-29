14e0899e96e0105f40ee7d1cedc2b0173340e14c
John Michael <jmichael@coverys.com>
2023-03-28 22:31:22 -0500

    check point getting data from api

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -2,84 +2,19 @@
 import './SearchResult.css';
 
 
-function SearchResult(props){
+
+function SearchResult({data}){
   
     return(
-<>
-<div className="container" style={{marginTop: '30px'}}>
-  <div className="row">
-    <div className="col-sm-4">
-    <div className="fakeimg">
-     <img src={props.medicinepicture} alt=''/>
-    </div>
-      <h2>Medicine</h2>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-      <p>Lorem ipsum dolor sit ame.</p>
-      <h3>Best picks</h3>
-    
-      
-      <ul className="nav nav-pills flex-column">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Active</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <hr className="d-sm-none" />
-    </div>
-    {/* Result table */}
-    <div className="col-sm-8">
-      <h2 className='ResultName' >Results</h2>
-     <div>
-     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Seller</th>
-      <th scope="col">location</th>
-      <th scope="col">price</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Walmart</td>
-      <td>boston</td>
-      <td>$20</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-     </div>
-      <br />
-      </div>
-  </div>
-  <div className="jumbotron text-center" style={{marginBottom: '0'}}>
-   
-  </div>
-</div>
 
-</>
-
+      <>
+      <p>
+        
+      </p>
+      </>
     );
 
 
 }
 export default SearchResult;
+
--- src/components/Searchform.js
+++ src/components/Searchform.js
@@ -1,22 +1,49 @@
+import React, { useState } from 'react';
 
+function Searchbar() {
+  const [searchQuery, setSearchQuery] = useState('');
 
-function Searchbar(){
-  
-    return(
-<>
-<div className="container-fluid">
-<div className="d-flex justify-content-center">
-  <form className="form-inline my-2 my-lg-0">
-    <div className="input-group">
-     <input className="form-control mr-sm-2 w-90" type="search" placeholder="Search" aria-label="Search"/>
-     <button className="btn btn-outline-success my-2 my-sm-0" type="submit"><i className="fas fa-search"></i></button>
+  const handleInputChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+
+    // Make a GET request to the API endpoint with the search query
+    fetch(`https://localhost:7070/api/medicines/Search?name=${searchQuery}`)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the search results
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="d-flex justify-content-center">
+        <form className="form-inline my-2 my-lg-0" onSubmit={handleFormSubmit}>
+          <div className="input-group">
+            <input
+              className="form-control mr-sm-2 w-90"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={searchQuery}
+              onChange={handleInputChange}
+            />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </form>
-</div>
-</div>
-</>
-    );
-
-
+  );
 }
+
 export default Searchbar;
